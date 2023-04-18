c902ad921f054b0e106874b9f5520e26001d15f2
AxelColliaux <axel.colliaux@hotmail.fr>
2023-04-18 22:58:47 +0200

    challenge ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './pages/auth/signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'signin' },
+  { path: 'signin', component: SigninComponent },
+  { path: 'video', loadChildren: () => import('./pages/video/video.module').then(m => m.VideoModule) },
+  { path: 'playlist', loadChildren: () => import('./pages/playlist/playlist.module').then(m => m.PlaylistModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
